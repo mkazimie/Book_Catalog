8808b9c5753b61f6e07a311e7ffa985767e0af74
Magdalena Kazimieruk <mkazimieruk@hotmail.com>
2020-03-31 21:09:08 +0200

    Add service classes

--- src/main/java/pl/coderslab/workshop5/service/BookService.java
+++ src/main/java/pl/coderslab/workshop5/service/BookService.java
@@ -1,4 +1,18 @@
 package pl.coderslab.workshop5.service;
 
+import pl.coderslab.workshop5.model.Book;
+
+import java.util.List;
+
 public interface BookService {
+
+    List<Book> getAllBooks();
+
+    Book getBookById(long id);
+
+    Book saveBook(Book book);
+
+    Book updateBook(long id, Book book);
+
+    void deleteBook (long id);
 }
--- src/main/java/pl/coderslab/workshop5/service/MemoryBookService.java
+++ src/main/java/pl/coderslab/workshop5/service/MemoryBookService.java
@@ -1,4 +1,65 @@
 package pl.coderslab.workshop5.service;
 
-public class MemoryBookService {
+import org.springframework.stereotype.Component;
+import pl.coderslab.workshop5.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class MemoryBookService implements BookService {
+
+    private List<Book> books;
+
+    public MemoryBookService() {
+        books = new ArrayList<>();
+        books.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel",
+                "Helion", "programming"));
+        books.add(new Book(2L, "9788324627738", "Rusz glowa, Java.",
+                "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        books.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    @Override
+    public Book getBookById(long id) {
+        Book foundBook = books.stream()
+                .filter(book -> book.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Nie ma takiej książki"));
+        return foundBook;
+    }
+
+
+    @Override
+    public Book saveBook(Book book) {
+        long nextId = books.stream()
+                .mapToLong(Book::getId)
+                .max()
+                .orElse(0) +1;
+        book.setId(nextId);
+        books.add(book);
+        return book;
+    }
+
+    @Override
+    public Book updateBook(long id, Book book) {
+        Book bookById = getBookById(id);
+        bookById.setAuthor(book.getAuthor());
+        bookById.setIsbn(book.getIsbn());
+        bookById.setTitle(book.getTitle());
+        bookById.setPublisher(book.getPublisher());
+        bookById.setType(book.getType());
+        return bookById;
+    }
+
+    @Override
+    public void deleteBook(long id) {
+        books.remove(getBookById(id));
+    }
 }
