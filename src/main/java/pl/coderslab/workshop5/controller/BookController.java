fc3c3ce916e8e53cbac3dc1ac9c6cb033ba6b666
Magdalena Kazimieruk <mkazimieruk@hotmail.com>
2020-03-31 21:06:54 +0200

    add controller

--- src/main/java/pl/coderslab/workshop5/controller/BookController.java
+++ src/main/java/pl/coderslab/workshop5/controller/BookController.java
@@ -1,4 +1,49 @@
 package pl.coderslab.workshop5.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.workshop5.model.Book;
+import pl.coderslab.workshop5.service.BookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> books() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable long id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        Book savedBook = bookService.saveBook(book);
+        return savedBook;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable long id) {
+        bookService.deleteBook(id);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable long id, @RequestBody Book book){
+        Book updatedBook = bookService.updateBook(id, book);
+        return updatedBook;
+    }
+
+
 }
